1e3a0f957cc938ca70700368af1bc5ef461cdae4
Aleksandr Kazakov <akazakov@avanpost.ru>
2017-10-27 19:13:45 +0300

    Add undirected graph

---
+++ Graph/UniGraph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graph
+{
+    public class UniGraph<T> : BaseGraph<T>
+    {
+        protected UniGraph(IEnumerable<GraphNode<T>> nodes) : base(nodes)
+        {
+        }
+
+        public override void AddEdge(GraphNode<T> from, GraphNode<T> to, int cost)
+        {
+            from.Neighbours.Add(to);
+            from.Costs.Add(cost);
+            to.Neighbours.Add(from);
+            to.Costs.Add(cost);
+
+        }
+
+        
+    }
+}
