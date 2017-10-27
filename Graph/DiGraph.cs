08191c178cd8e263173d29f37ef96b98ea87a52b
Aleksandr Kazakov <akazakov@avanpost.ru>
2017-10-27 19:17:06 +0300

    Add directed graph

---
+++ Graph/DiGraph.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph
+{
+    public class DiGraph<T> : BaseGraph<T>
+    {
+        protected DiGraph(IEnumerable<GraphNode<T>> nodes) : base(nodes)
+        {
+        }
+
+        public override void AddEdge(GraphNode<T> from, GraphNode<T> to, int cost)
+        {
+            from.Neighbours.Add(to);
+            from.Costs.Add(cost);
+        }
+    }
+}
